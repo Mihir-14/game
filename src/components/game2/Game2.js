741b9e425df4c4da6d8baf5ad79b92ad80c2ca9d
Mihir Dudhatra <mihirdudhatra3906@gmail.com>
2024-09-16 17:03:35 +0530

    random operator updated

--- src/components/game2/Game2.js
+++ src/components/game2/Game2.js
@@ -5,58 +5,101 @@ const Game2 = ({ deductMoney, addMoney }) => {
   const [userNumber, setUserNumber] = useState('');
   const [computerNumber, setComputerNumber] = useState(null);
   const [result, setResult] = useState('');
+  const [operator, setOperator] = useState('');
+  const [stage, setStage] = useState(1); 
   const navigate = useNavigate();
+
+  const generateRandomOperator = () => {
+    const operators = ['<', '>', '='];
+    const randomIndex = Math.floor(Math.random() * operators.length);
+    return operators[randomIndex];
+  };
+
+  const handleStartGame = () => {
+    setOperator(generateRandomOperator());
+    setStage(2);
+  };
+
   const handleInput = (e) => {
     setUserNumber(e.target.value);
-  }
+  };
+
   const handleAction = () => {
     const number = parseInt(userNumber, 10);
-    if(isNaN(number) || number < 1 || number > 10){
-      alert("Please Enter the number in Range: 1 to 10");
+    if ( number < 1 || number > 10 ) {
+      alert('Please Enter the number in Range: 1 to 10');
       return;
     }
 
     const compNum = Math.floor(Math.random() * 10) + 1;
+    const newOperator = generateRandomOperator();
+    
     setComputerNumber(compNum);
+    setOperator(newOperator);
 
-    if(number > compNum){
-      addMoney(10);
-      setResult(`Number: ${number} is greater than ${compNum} You Won!!`);
-    }else if(number < compNum){
-      deductMoney(10);
-      setResult(`Number: ${number} is less than ${compNum} You lose`);
-    }else{
-      setResult(`Number: ${number} is equal to ${compNum} Match is tied`);
+    let ans;
+    switch (newOperator) {
+      case '<':
+        ans = number < compNum;
+        break;
+      case '>':
+        ans = number > compNum;
+        break;
+      case '=':
+        ans = number === compNum;
+        break;
+      default:
+        ans = false;
     }
 
+    if (ans) {
+      if(newOperator === '='){
+        addMoney(20);
+        setResult(`Number: ${number} ${newOperator} ${compNum} You Won!!`);
+      }
+      addMoney(10);
+      setResult(`Number: ${number} ${newOperator} ${compNum} You Won!!`);
+    } else {
+      deductMoney(10);
+      setResult(`Number: ${number} ${newOperator} ${compNum} You Lose`);
+    }
+    setUserNumber('');
   };
+
   const goToMainMenu = () => {
     navigate('/');
   };
 
-
-
   return (
     <div className="container text-center">
       <h1 className="display-4 mb-4">Welcome to Game 2</h1>
-      <div className='mb-4'>
-        <input 
-        type='number'
-        min={1}
-        max={10}
-        value={userNumber}
-        onChange={handleInput}
-        className='form-control mb-2'
-        placeholder='Enter Your fav number between 1 to 10'
-        />
-        <button className="btn btn-info btn-lg mb-2" onClick={handleAction}>Play Game 2</button>
-      </div>
+      {stage === 1 && (
+        <div className='mb-4'>
+          <button className="btn btn-info btn-lg mb-2" onClick={handleStartGame}>Start Game</button>
+        </div>
+      )}
+      {stage === 2 && (
+        <div>
+          <h2>Operator: {operator}</h2>
+          <div className='mb-4'>
+            <input
+              type='number'
+              min={1}
+              max={10}
+              value={userNumber}
+              onChange={handleInput}
+              className='form-control mb-2'
+              placeholder='Enter Your fav number between 1 to 10'
+            />
+            <button className="btn btn-info btn-lg mb-2" onClick={handleAction}>Play Game 2</button>
+          </div>
+        </div>
+      )}
       {computerNumber !== null && (
         <div className='mb-4'>
-          <p>Computer's Number : {computerNumber}</p>
+          <p>Computer's Number: {computerNumber}</p>
           <p>{result}</p>
         </div>
-
       )}
       <button className="btn btn-secondary btn-lg" onClick={goToMainMenu}>Back to Main Menu</button>
     </div>
